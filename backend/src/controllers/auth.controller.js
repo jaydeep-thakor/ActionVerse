7046d10a1c13decadeb27f75771f5b32a738badd
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-05-22 23:11:14 +0530

    add login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import userModel from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import config from "../config/config.js";
+import jwt from "jsonwebtoken"
 
 const signUp = async (req, res) => {
 
@@ -58,16 +60,72 @@ const signUp = async (req, res) => {
         console.log("error", error);
 
         return res.status(500).json({
-            message: "internal server error"
+            message: "Internal server error"
         })
 
     }
 
 }
 
-// const signIn = (req, res) => {
-//     console.log("SignIn controller");
-// }
+const signIn = async (req, res) => {
 
-export { signUp };
+    try {
+        
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                message: "All fields are required"
+            })
+        }
+
+        const user = await userModel.findOne({
+            $or: [
+                {email: email},
+                {password: password}
+            ]
+        })
+
+        if(!user){
+            return res.status(404).json({
+                message: "User does not exists"
+            })
+        }
+
+        const decodedPassword = await bcrypt.compare(password, user.password);
+
+        if(!decodedPassword){
+            return res.status(401).json({
+                message: "Invalid credentials"
+            })
+        }
+
+        const token = await jwt.sign({
+            id: user._id
+        }, config.JWT_SECRET)
+
+        res.cookie("userToken", token);
+
+        return res.status(200).json({
+            message: "Login successfullys",
+            user: {
+                id: user._id,
+                phoneNumber: user.phoneNumber,
+                fullName: user.fullName,
+                email: user.email
+            }
+        })
+
+    } catch (error) {
+
+        console.log("error", error);
+
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+
+    }
+}
+
+export { signUp, signIn };
 
