7fbc1ba34295cef5a79989f369047e26c113c1ef
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-05-27 21:47:57 +0530

    add movie page route

--- frontend/src/components/home/Movies.jsx
+++ frontend/src/components/home/Movies.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Title from '../Title'
 import img from "../../assets/images/poster.jpg"
+import useNavigate from 'react-router-dom'
 
 const Movies = () => {
+
+  const navigate = useNavigate();
 
   let number = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
 
@@ -15,7 +18,9 @@ const Movies = () => {
         {
           number.map((item, index) => {
             return (
-              <div key={index} className='rounded-xl border border-gray-100 bg-white transition-transform hover:-translate-y-1 hover:border-gray-300 cursor-pointer'>
+              <div onClick={()=>{
+                navigate("/show");
+              }} key={index} className='rounded-xl border border-gray-100 bg-white transition-transform hover:-translate-y-1 hover:border-gray-300 cursor-pointer'>
                 <img className='aspect-3/4 w-full object-cover' src={img} alt="" />
                 <div className='p-3 flex flex-col gap-1'>
                   <h5 className='font-semibold text-lg leading-none'>Lorem, ipsum dolor.</h5>
